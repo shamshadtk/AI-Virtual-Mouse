2dfc033cc4454bd40e53c1c4bf1b868b7f7d2077
Shamshad Thonikkara <shamshadthonikkara@gmail.com>
2023-04-01 12:58:28 +0530

    modified file

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -7,8 +7,8 @@ import pyautogui as ppy
 
 ######################
 wCam, hCam = 500, 300
-frameR = 100     #Frame Reduction
-smoothening = 7  #random value
+frameR = 100  # Frame Reduction
+smoothening = 7  # random value
 ######################
 
 pTime = 0
@@ -20,7 +20,6 @@ cap.set(4, hCam)
 
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
-
 # print(wScr, hScr)
 
 while True:
@@ -40,7 +39,7 @@ while True:
                       (255, 0, 255), 2)
 
         # Only Index Finger: Moving Mode
-        if fingers==[0, 1, 0, 0, 0]:
+        if fingers == [0, 1, 0, 0, 0]:
 
             # Convert the coordinates
             x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
@@ -56,14 +55,15 @@ while True:
             plocX, plocY = clocX, clocY
 
         # Both Index and middle are up: Left Clicking Mode
-        if fingers== [0, 1, 1, 0, 0]:
+        if fingers == [0, 1, 1, 0, 0]:
 
             # Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
 
             # Click mouse if distance short
             if length < 40:
-                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]),
+                           15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
 
         # Both Index and thumb are up: Right Clicking Mode
@@ -81,45 +81,19 @@ while True:
                 autopy.mouse.click(autopy.mouse.Button.RIGHT)
 
         # if three Fingers: scrolling up Mode
-        if fingers==[0, 1, 1, 1, 0]:
-
-            # Convert the coordinates
-            # x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
-            # y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
-
-            # Smooth Values
-            # clocX = plocX + (x3 - plocX) / smoothening
-            # clocY = plocY + (y3 - plocY) / smoothening
-
-            # Move Mouse
-            # autopy.mouse.move(wScr - clocX, clocY)
+        if fingers == [0, 1, 1, 1, 0]:
             ppy.scroll(-30)
-            # cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            # plocX, plocY = clocX, clocY
 
         # if four Fingers: scrolling down Mode
-        if fingers==[0, 1, 1, 1, 1]:
-
-            # Convert the coordinates
-            # x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
-            # y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
-
-            # Smooth Values
-            # clocX = plocX + (x3 - plocX) / smoothening
-            # clocY = plocY + (y3 - plocY) / smoothening
-
-            # Move Mouse
-            # autopy.mouse.move(wScr - clocX, clocY)
+        if fingers == [0, 1, 1, 1, 1]:
             ppy.scroll(30)
-            # cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            # plocX, plocY = clocX, clocY
-
 
     # Frame rate
     cTime = time.time()
-    # fps = 1/(cTime-pTime)
+    fps = 1/(cTime-pTime)
     pTime = cTime
-    # cv2.putText(img, str(int(fps)), (28, 58), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
+    cv2.putText(img, str(int(fps)), (10, 20),
+                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
     # Display
     cv2.imshow("Image", img)
